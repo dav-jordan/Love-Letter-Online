c49f09b757a4170ed97f78ea3e822a3fb24c6021
Davis Jordan <jorda128@purdue.edu>
2018-11-29 12:33:13 -0500

    room creation/joining handlers

---
+++ scripts/roomHandling.js
@@ -0,0 +1,13 @@
+function joinRoom() {
+  let username = document.getElementById("username").value;
+  let lobby = document.getElementById("lobby").value;
+  console.log("username: " + username);
+  console.log("joining room " + lobby);
+}
+
+function createRoom() {
+  let username = document.getElementById("username").value;
+  let lobby = document.getElementById("lobby").value;
+  console.log("username: " + username);
+  console.log("creating room " + lobby);
+}
