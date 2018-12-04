df13443d988dc19cd4c5f06f9cb10ce53fa9df60
Davis Jordan <jorda128@purdue.edu>
2018-12-04 14:27:02 -0500

    card discarding

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,10 @@
-var playerCard;
+var playerCard = "<img id=\"pCard1\" src=\"images/cards/guard.jpg\" width=\"150\" height=\"200\"alt=\"card\" />";
 var opponents = [
   "<img src=\"images/cards/face-down.jpeg\" width=\"150\" height=\"200\"alt=\"card\" />",
   "<img src=\"images/cards/face-down.jpeg\" width=\"150\" height=\"200\"alt=\"card\" />",
   "<img src=\"images/cards/face-down.jpeg\" width=\"150\" height=\"200\"alt=\"card\" />"
 ]
+var takingTurn = false;
 
 function opponentCards() {
   console.log("displaying opponent cards");
@@ -22,15 +23,56 @@ function opponentCards() {
 function playerCards() {
   console.log("displaying player card");
   //retrieve player card from server
-  playerCard =
-    "<img src=\"images/cards/guard.jpg\" width=\"150\" height=\"200\"alt=\"card\" />";
   document.getElementById("playerCards").innerHTML = playerCard;
 }
 
+function playersTurn() {
+  //set takingTurn to true to prevent update
+  takingTurn = true;
+
+  //draw another card
+  let nCard =
+    "<input id=\"pCard2\" onclick=\"discard2()\" type=\"image\" src=\"images/cards/baron.jpg\" width=\"150\" height=\"200\"alt=\"card\" />";
+  playerCard =
+  "<input id=\"pCard1\"  onclick=\"discard1()\" type=\"image\" src=\""
+  + document.getElementById("pCard1").src + "\" width=\"150\" height=\"200\"alt=\"card\" />"
+  + nCard;
+
+  document.getElementById("playerCards").innerHTML = playerCard;
+}
+
+//updates state every 2 seconds
 function update() {
   opponentCards();
   playerCards();
 
+  console.log(takingTurn);
+  if(takingTurn)
+    return;
+  //check if it is the player's turn
+  let playerTurn = true;
+  if(playerTurn){
+    console.log("player");
+    playersTurn();
+    playerTurn = false;
+  } else {
+    console.log("opponent");
+  }
+
   //update every two seconds
   setTimeout(update, 2000);
+}
+
+function discard1() {
+  console.log("discarding 1");
+  playerCard =
+  "<img  id=\"pCard1\" src=\"" + document.getElementById("pCard2").src + "\" "
+  + "width=\"150\" height=\"200\"alt=\"card\" />";
+  takingTurn = false;
+  update();
+  // setTimeout(discard1, 2000);
+}
+function discard2() {
+  console.log("discarding 2");
+  // setTimeout(discard2, 2000);
 }
